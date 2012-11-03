cf2f43f30ffbb83388e71c6a8a0e02ef8dc3f124
rvk0106 <rvk0106@gmail.com>
2012-11-04 00:44:11 +0530

    Update src/ravindhra_project/Ravindhra_project.java
    ghjgkkj

--- src/ravindhra_project/Ravindhra_project.java
+++ src/ravindhra_project/Ravindhra_project.java
@@ -14,6 +14,6 @@ public class Ravindhra_project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hai this is vamsi");
+        System.out.print("Hai this is vamsi krishna");
     }
 }
