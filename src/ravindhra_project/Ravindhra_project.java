78445e22688bcb316efe0a52acdfb814321bd7f2
tele6 <tele6@tele6-desktop>
2012-11-03 17:03:09 +0530

    Test Commit of  vamsi

--- src/ravindhra_project/Ravindhra_project.java
+++ src/ravindhra_project/Ravindhra_project.java
@@ -14,6 +14,6 @@ public class Ravindhra_project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Babu Lokesh pakkakelli aduko amma.");
     }
 }
