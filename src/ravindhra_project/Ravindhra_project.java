c525f0cb039dbb1db3f231acb2ea1d142de75460
Praveen <Praveen@Praveen-PC>
2012-11-04 00:42:33 +0530

    msg

--- src/ravindhra_project/Ravindhra_project.java
+++ src/ravindhra_project/Ravindhra_project.java
@@ -14,6 +14,6 @@ public class Ravindhra_project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Babu Lokesh pakkakelli aduko amma.");
+        System.out.print("Hai this is vamsi");
     }
 }
